87538c990f3f9f92227f5d83b810540dc945eac3
Natanel-Shitrit <natanelsht@gmail.com>
2021-12-04 23:26:36 +0200

    Remove `mms` option from from AMBuild

--- configure.py
+++ configure.py
@@ -26,8 +26,6 @@ builder = run.PrepareBuild(sourcePath = sys.path[0])
 
 builder.options.add_option('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
 		                   help='Root search folder for HL2SDKs')
-builder.options.add_option('--mms-path', type=str, dest='mms_path', default=None,
-                       help='Path to Metamod:Source')
 builder.options.add_option('--sm-path', type=str, dest='sm_path', default=None,
                        help='Path to SourceMod')
 builder.options.add_option('--enable-debug', action='store_const', const='1', dest='debug',
